762c939b272f38a511c230c90c941bc628cf110a
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-17 22:00:33 -0300

    login with valid data test

---
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -0,0 +1,24 @@
+package br.com.alura.leilao.login;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	@Test
+	public void should_login_with_valid_data() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:8080/login");
+		browser.findElement(By.id("username")).sendKeys("fulano");
+		browser.findElement(By.id("password")).sendKeys("pass");
+		browser.findElement(By.id("login-form")).submit();
+		
+		Assert.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+		Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
+		browser.quit();
+	}
+}
