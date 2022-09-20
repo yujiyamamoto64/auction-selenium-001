c3a4106a2fc058d5363e564ebfddb1d2d4180db6
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-20 18:42:16 -0300

    Abrasileirando o cucumber. #language: pt

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -7,31 +7,30 @@ import org.junit.Assert;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
 
 public class PropondoLanceSteps {
 	
 	private Lance lance;
 	private Leilao leilao;
 
-	@Given("Dado um lance valido")
+	@Dado("um lance valido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("fulano");
 		lance = new Lance(usuario, BigDecimal.TEN);
 	}
 	
-	@When("Quando propoe o lance")
+	@Quando("propoe o lance")
 	public void quando_propoe_o_lance() {
 		leilao = new Leilao("Tablet XPTO");
 		leilao.propoe(lance);
 	}
 	
-	@Then("Entao o lance eh aceito")
+	@Então("o lance eh aceito")
 	public void entao_o_lance_eh_aceito() {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
 	}
-	
 }
--- src/test/resources/features/propondo_laces.feature
+++ src/test/resources/features/propondo_laces.feature
@@ -1,6 +1,8 @@
-Feature: Propondo lances
+# language: pt
+
+Funcionalidade: Propondo lances
   
-  Scenario: Propondo um unico lance valido
-    Given Dado um lance valido
-    When Quando propoe o lance
-    Then Entao o lance eh aceito
+  Cenário: Propondo um unico lance valido
+    Dado um lance valido
+    Quando propoe o lance
+    Entao o lance eh aceito
