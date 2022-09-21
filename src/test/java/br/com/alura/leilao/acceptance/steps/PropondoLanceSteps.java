7493524293dcf11c63c46992c484902d99d8e227
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-21 10:18:59 -0300

    Testando varios lances

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -12,25 +12,49 @@ import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
 public class PropondoLanceSteps {
-	
+
 	private Lance lance;
 	private Leilao leilao;
+	private Lance lance15;
+	private Lance lance10;
 
 	@Dado("um lance valido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("fulano");
 		lance = new Lance(usuario, BigDecimal.TEN);
 	}
-	
+
 	@Quando("propoe o lance")
 	public void quando_propoe_o_lance() {
 		leilao = new Leilao("Tablet XPTO");
 		leilao.propoe(lance);
 	}
-	
+
 	@Então("o lance eh aceito")
 	public void entao_o_lance_eh_aceito() {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+	}
+
+	@Dado("varios lances validos")
+	public void varios_lances_validos() {
+		Usuario usuario1 = new Usuario("fulano");
+		lance10 = new Lance(usuario1, BigDecimal.TEN);
+		Usuario usuario2 = new Usuario("beltrano");
+		lance15 = new Lance(usuario2, new BigDecimal("15.0"));
+		leilao = new Leilao("Tablet XPTO");
+	}
+
+	@Quando("propoe varios lances ao leilao")
+	public void propoe_varios_lances_ao_leilao() {
+		leilao.propoe(lance10);
+		leilao.propoe(lance15);
+	}
+
+	@Então("os lances sao aceitos")
+	public void os_lances_sao_aceitos() {
+		Assert.assertEquals(2, leilao.getLances().size());
+		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+		Assert.assertEquals(new BigDecimal("15.0"), leilao.getLances().get(1).getValor());
 	}
 }
--- src/test/resources/features/propondo_laces.feature
+++ src/test/resources/features/propondo_laces.feature
@@ -1,8 +1,13 @@
 # language: pt
 
-Funcionalidade: Propondo lances
+Funcionalidade: Propondo lances ao leilao
   
   Cenário: Propondo um unico lance valido
     Dado um lance valido
     Quando propoe o lance
     Entao o lance eh aceito
+    
+  Cenário: Propondo varios lances validos
+  	Dado varios lances validos
+  	Quando propoe varios lances ao leilao
+  	Entao os lances sao aceitos
